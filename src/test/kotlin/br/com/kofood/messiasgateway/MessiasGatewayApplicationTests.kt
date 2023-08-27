43310fad9a9399f9b0459a9ca4142bc21dd2d34d
ocartaxo <oacartax@gmail.com>
2023-08-27 16:24:54 -0300

    default tests

---
+++ src/test/kotlin/br/com/kofood/messiasgateway/MessiasGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.kofood.messiasgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessiasGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
